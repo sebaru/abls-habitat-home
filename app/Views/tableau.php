c1d4ee18d7c97044cfeee4904206f68a703cfa43
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2023-07-13 07:46:28 +0200

    TABLEAU: ADD Tableau.php

---
+++ app/Views/tableau.php
@@ -0,0 +1,12 @@
+<main role="main" class="container-fluid">
+
+<section id='idSectionTableaux' class='row mx-1 row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3 justify-content-center'>
+
+<canvas id='idTableauCanvas' class="col wtd-courbe m-1"></canvas>
+
+</section>
+
+</main>
+
+
+<script src="/js/tableau.js" type="text/javascript"></script>
