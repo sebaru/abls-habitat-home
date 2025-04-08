1d53e41d2664cd0036cbc46bb0c114e24bb4edc2
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2025-04-08 19:13:12 +0200

    CI: Move 'default' route to 'index'

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/domains',        'Home::domains');
-$routes->get('/messages',       'Home::messages');
-$routes->get('/historique',     'Home::historique');
-$routes->get('/test',           'Home::test');
-$routes->get('(:any)',          'Home::default');
+$routes->get('/domains',    'Home::domains');
+$routes->get('/messages',   'Home::messages');
+$routes->get('/historique', 'Home::historique');
+$routes->get('/test',       'Home::test');
+$routes->get('/(:any)',     'Home::index');
