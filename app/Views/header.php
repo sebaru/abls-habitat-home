8a8dee1647cfa44eccfb7b8536ea0328f72f8362
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-04-28 19:53:31 +0200

    FEAT: Change dropdown alignment from right to end in header.php

--- app/Views/header.php
+++ app/Views/header.php
@@ -316,7 +316,7 @@
           <i class="fas fa-user text-white"></i> <span id="idUsername">-</span>
         </a>
 
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarUSER">
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarUSER">
           <a class="dropdown-item" href="/domains"> <i class="fas fa-globe text-primary"></i> Mes domaines</a>
           <a class="dropdown-item" href="/messages"> <i class="fas fa-book text-primary"></i> Fil de l'eau</a>
           <a class="dropdown-item" href="/historique"> <i class="fas fa-history text-primary"></i> Historique</a>
