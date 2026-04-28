0809378ce94186df0fe21d94d806bd9448bc6b36
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-04-28 19:50:20 +0200

    FEAT: Fix syntax error in beforeSend function for X-ABLS-DOMAIN header in home.js, messages.js, and tableau.js

--- public/js/home.js
+++ public/js/home.js
@@ -133,7 +133,7 @@
                   data: function (d) { d.syn_page=Synoptique.page },
                   error: function ( xhr, status, error ) { Show_toast_ko(xhr.statusText); },
                   beforeSend: function (request)
-                                 request.setRequestHeader('X-ABLS-DOMAIN', localStorage.getItem('domain_uuid') );
+                                 { request.setRequestHeader('X-ABLS-DOMAIN', localStorage.getItem('domain_uuid') );
                                }
                 },
           rowId: "histo_msg_id",
