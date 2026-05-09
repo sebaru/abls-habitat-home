9f728d6008be4ece8c1d15f44b67e27e46ffa0ff
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-05-09 11:13:42 +0200

    FEAT: Update API endpoints in JavaScript files and add new config.js for IDP settings

--- public/js/home.js
+++ public/js/home.js
@@ -129,7 +129,7 @@
        else $("#idTableMessages").empty().DataTable(
         { pageLength : 50,
           fixedHeader: true, paging: false, ordering: true, searching: true,
-          ajax: { url : $ABLS_API+"/histo/alive", type : "GET", dataSrc: "histo_msgs", contentType: "application/json",
+          ajax: { url : "/api/histo/alive", type : "GET", dataSrc: "histo_msgs", contentType: "application/json",
                   data: function (d) { d.syn_page=Synoptique.page },
                   error: function ( xhr, status, error ) { Show_toast_ko(xhr.statusText); },
                   beforeSend: function (request)
