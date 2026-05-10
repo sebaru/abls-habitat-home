9cc66033ccf20a6ba8dffcfd9d5b68f364af1da3
Sébastien Lefèvre <sebastien.lefevre@abls-habitat.fr>
2026-05-10 08:39:44 +0200

    FEAT: Remove deprecated IDP configuration from config.js

--- public/js/config.js
+++
@@ -1,5 +0,0 @@
-var $IDP_ADAPTER   = "https://dev-idp.abls-habitat.fr/js/keycloak.js";
-var $IDP_REALM     = "dev-Abls-Habitat";
-var $IDP_URL       = "https://dev-idp.abls-habitat.fr/";
-var $IDP_CLIENT_ID = "dev-abls-habitat-home";
-
